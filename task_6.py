d6d61504bc1cf968980c391382680ff6e6785174
gweingil <gweingil@gmail.com>
2021-10-13 20:27:23 +0600

    created new tasks

---
+++ task_6.py
@@ -0,0 +1,18 @@
+result = {}
+with open('subjects.txt') as file:
+    file_lines = file.readlines()
+    for line in file_lines:
+        data = line.split()
+        hours = 0
+        for elem in data[1:]:
+            if elem !=  '-':
+                num = '0'
+                for i in elem:
+                    if i.isdigit():
+                        num += i
+                    else:
+                        break
+                hours += int(num)
+        result.update({data[0].strip(':'): hours})
+
+print(result)
